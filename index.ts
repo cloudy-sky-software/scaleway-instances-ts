d5be6b3dbf0449a10a0f68403474785dbeb47a41
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2022-11-12 10:44:58 +0530

    Add stack config file. Add a Server resource

--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as instances from "@cloudyskysoftware/pulumi-scaleway-instances";
+
+// Paris 2 zone is environmentally friendly.
+const zone = "fr-par-2";
+
+const images = pulumi.output(
+    instances.images.listImages({
+        zone,
+    })
+);
+
+const server = new instances.servers.Server("testServer", {
+    name: "my-test-server",
+    commercial_type: "PLAY2-PICO",
+    boot_type: "local",
+    dynamic_ip_required: false,
+    enable_ipv6: true,
+    image: images.items.images?.apply((i) => {
+        const ubuntu = i?.filter(
+            (img) =>
+                img.arch === "x86_64" &&
+                img.name === "Ubuntu 22.04 Jammy Jellyfish"
+        )[0];
+
+        return ubuntu!.id!;
+    }),
+    project: "65978379-535c-45a1-87be-92e4e3926db5",
+    volumes: {
+        0: {
+            volume_type: "b_ssd",
+            size: 10 * 1000 * 1000 * 1000,
+        },
+    },
+    zone,
+});
