8740715db57b96149ea220ee25ffd2f385d2a321
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2022-11-12 16:34:27 +0530

    Working app

--- index.ts
+++ index.ts
@@ -3,12 +3,21 @@ import * as instances from "@cloudyskysoftware/pulumi-scaleway-instances";
 
 // Paris 2 zone is environmentally friendly.
 const zone = "fr-par-2";
+const project = "65978379-535c-45a1-87be-92e4e3926db5";
 
 const images = pulumi.output(
     instances.images.listImages({
         zone,
     })
 );
+
+const osVolume = new instances.volumes.Volume("osVolume", {
+    name: "osVolume",
+    project,
+    size: 10 * 1000 * 1000 * 1000,
+    volume_type: "b_ssd",
+    zone,
+});
 
 const server = new instances.servers.Server("testServer", {
     name: "my-test-server",
@@ -25,12 +34,18 @@ const server = new instances.servers.Server("testServer", {
 
         return ubuntu!.id!;
     }),
-    project: "65978379-535c-45a1-87be-92e4e3926db5",
+    project,
     volumes: {
         0: {
-            volume_type: "b_ssd",
-            size: 10 * 1000 * 1000 * 1000,
+            id: osVolume.id,
+            name: "System volume",
         },
     },
+    zone,
+});
+
+new instances.action.ServerAction("poweroffAction", {
+    server_id: server.id,
+    action: "poweroff",
     zone,
 });
